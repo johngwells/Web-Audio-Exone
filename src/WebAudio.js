74aef3275aea55ad403c091428daefd129e1ec17
John Wells <molekewl2017@gmail.com>
2019-09-22 14:25:08 -0700

    create WebAudio.js & set up AudioContext to process audio

--- src/WebAudio.js
+++ src/WebAudio.js
@@ -1,6 +1,17 @@
 import React from 'react';
 
 function WebAudio() {
+
+  /*
+    This gives access to the Web Audio API
+    When using Audio Context a BaseAudioContext is created to allow online & offline audio processing
+    You wouldn't use that directly but by using it's features
+    https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext
+  */
+
+  const AudioContext = window.AudioContext || window.webkitAudioContext;
+  const audioContext = new AudioContext();
+
   return (
     <div>
       <p>React Web Audio Api</p>
